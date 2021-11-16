d4b99bd45f27ce28e714735b488d3668ef5d7aeb
FiNeX <287009+finex@users.noreply.github.com>
2021-11-16 09:42:26 +0100

    Put right modifiers on right hand (except ALT)

--- keymap.c
+++ keymap.c
@@ -263,7 +263,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        _______ ,KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX ,KC_COLN, KC_DLR  ,KC_PERC ,KC_CIRC ,KC_PLUS ,ASC_SAR ,                          ASC_DAR ,XXXXXXX ,KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI ,XXXXXXX ,
+       XXXXXXX ,KC_COLN, KC_DLR  ,KC_PERC ,KC_CIRC ,KC_PLUS ,ASC_SAR ,                          ASC_DAR ,XXXXXXX ,KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI ,XXXXXXX ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        XXXXXXX ,KC_TILD, KC_EXLM ,KC_AT   ,KC_HASH ,KC_PIPE ,ASC_SAD ,ASC_HAP ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
     //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
@@ -278,7 +278,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        _______ ,KC_LBRC ,KC_7    ,KC_8    ,KC_9    ,KC_RBRC ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX ,KC_SCLN ,KC_4    ,KC_5    ,KC_6    ,KC_LSCR ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI ,XXXXXXX ,
+       XXXXXXX ,KC_SCLN ,KC_4    ,KC_5    ,KC_6    ,KC_LSCR ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI ,XXXXXXX ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        XXXXXXX ,KC_DQUO ,KC_1    ,KC_2    ,KC_3    ,KC_BSLS ,XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
     //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
@@ -293,7 +293,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        _______ ,KC_F12  ,KC_F7   ,KC_F8   ,KC_F9   ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX ,KC_F11  ,KC_F4   ,KC_F5   ,KC_F6   ,KC_CAPS ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI ,XXXXXXX ,
+       XXXXXXX ,KC_F11  ,KC_F4   ,KC_F5   ,KC_F6   ,KC_CAPS ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI ,XXXXXXX ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        XXXXXXX ,KC_F10  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_INS  ,XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_ALGR ,XXXXXXX ,XXXXXXX ,
     //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
